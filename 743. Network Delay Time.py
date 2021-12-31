bc5c87600271516a72ce20ba297de4762f2d41a3
yuf7605 <73343346+wuyufan7605@users.noreply.github.com>
2021-12-31 22:28:54 +0800

    743. Network Delay Time.py
    使用Dijkstra演算法跟minHeap
    從題目給的起點開始,一一去找起點的鄰居,然後把鄰居的路徑長,跟鄰居
    分別存成key和value,存進minHeap
    再從minHeap中找出最小路徑者,pop出來
    最小路徑的那個點就是下一個拜訪的點
    
    把下一個拜訪的點,當下一次的起點
    重複剛剛的過程
    就可以找到最小路徑長:)

---
+++ 743. Network Delay Time.py
@@ -0,0 +1,32 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        edges=collections.defaultdict(list)
+        #把所有的路徑存進edges中
+        for u,v,w in times:
+            edges[u].append((v,w))
+        #把起點先放進minHeap中
+        minHeap=[(0,k)]
+        #用set就部會重複存走過的點
+        visit=set()
+        #先把現在總路徑長設為0
+        cur_path=0
+        while minHeap:
+            #把目前為止最小路徑的點pop出來
+            w1,n1=heapq.heappop(minHeap)
+            #如果已經拜訪過了,就繼續
+            if n1 in visit:
+                continue
+            #如果還沒拜訪過,那現在拜訪了,所以加進visit中
+            visit.add(n1)
+            #上一回跟這一回的路徑誰大,就更新誰
+            cur_path=max(cur_path,w1) 
+            
+            #BFS
+            #n1是現在所在的這個點
+            for nei,nei_weight in edges[n1]:
+                if nei not in visit:
+                    #把weight放前面,因為要選weight最小者
+                    heapq.heappush(minHeap,(w1+nei_weight,nei))
+        return cur_path 
+            
+            
