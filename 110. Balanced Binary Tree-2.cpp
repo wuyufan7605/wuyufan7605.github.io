14dbd7d582de01de68e7f31e4e08ff6d9c9faa26
yuf7605 <73343346+wuyufan7605@users.noreply.github.com>
2022-01-03 18:32:13 +0800

    110. Balanced Binary Tree-2.cpp
    這個方法
    時間複雜度為:O(N)
    不像上個方法,主程式跟副程式都有調動(逐個算高度,跟遞迴)
    所以比較花時間
    //這個方法,只有在副程式調動
    會看每個點的左右子樹是否平衡
    所以時間為:o(N)

---
+++ 110. Balanced Binary Tree-2.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //ans表示是否平衡
+    bool ans;
+    //這個函式用來確認是否平衡
+    //會不斷遞回
+    int checkBalance(TreeNode* root){
+        if(!root) 
+            return 0;
+        if(!ans) //如果答案已經錯了且已經回傳
+            return 0;
+        //往左右子樹遞回
+        int leftSubTree=checkBalance(root->left);
+        int rightSubTree=checkBalance(root->right);
+        if(abs(leftSubTree-rightSubTree)>1){
+            ans=false;
+        }
+        //算每個點的高度
+        return 1+max(leftSubTree,rightSubTree);
+    }
+    //這邊為主程式
+    //一開始設有平衡
+    //若不符合再改成false
+    bool isBalanced(TreeNode* root){
+        ans=true;
+        int temp=checkBalance(root);
+        return ans;
+    }
+
+};
