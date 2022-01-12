f99a607d2b05937986ef4c6a60d9560f2882b4cc
yuf7605 <73343346+wuyufan7605@users.noreply.github.com>
2022-01-12 13:23:24 +0800

    101. Symmetric Tree.cpp
    主要分為:
    (1)主程式:
                 (a)若沒有root->對稱
                 (b)看左右子點否對稱(使用副程式)
    (2)副程式
                看左右子點的值是否相同
                 再往下一層檢查:左子點之左子點的值==右子點之右子點的值
                                            左子點之右子點的值==右子點之左子點的值  
    如圖所示:
      https://i.imgur.com/vo2yZhX.jpg

---
+++ 101. Symmetric Tree.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        //如果沒有root也是對稱
+        if(!root)
+            return true;
+        //看左右子樹是否對稱
+        return areMirror(root->left,root->right);
+    }
+    //副程式:看左右子樹是否對稱
+    bool areMirror(TreeNode* t1,TreeNode* t2){
+        //要有左右子點才行(不可其一為空)
+        if(!t1||!t2){
+            return(t1==t2);
+        }
+        //左右子點的值要一樣
+        if(t1->val==t2->val){
+            return true;
+        }
+        //繼續往下層的子點檢查:是否對稱
+        return (areMirror(t1->left,t2->right)&&areMirror(t1->right,t2->left));
+    }
+};
