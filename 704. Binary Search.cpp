6a817e2cbbca983ffd76f836818ab555491af786
yu7605 <73343346+wuyufan7605@users.noreply.github.com>
2022-03-27 17:03:59 +0800

    704. Binary Search.cpp
    分為兩個部分:
    (1)副程式:bSearch,做二分搜尋
    (2)主程式 search,呼叫使用副程式
    
    There are two functions:
    (1)minor function: bSearch
      I use the minor function to do binary search.
    (2)major function: search
        The major function uses the minor function.

---
+++ 704. Binary Search.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int bSearch(vector<int>&nums,int target,int start,int end){
+        if(end<start) return -1;
+        int mid=(start+end)/2;
+        
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]>target){
+            return bSearch(nums,target,start,mid-1);
+        }else{
+            return bSearch(nums,target,mid+1,end);
+        }
+    }
+    int search(vector<int>& nums, int target) {
+        return bSearch(nums,target,0,nums.size()-1);
+    }
+};
